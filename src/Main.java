bce1545c263dd555078dc31a2266a4c58f222dab
melisbm <miquelangel.melis@gmail.com>
2026-03-06 15:59:34 +0100

    Use Shape type class to declare shapes

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ import Shapes.Level1.*;
 public class Main {
     public static void main(String[] args){
 
-        Rectangle rectangle = new Rectangle(4, 5);
-        Circle circle = new Circle(4);
-        Triangle triangle = new Triangle(4);
+        Shape rectangle = new Rectangle(4, 5);
+        Shape circle = new Circle(4);
+        Shape triangle = new Triangle(4);
 
         System.out.println(rectangle.getArea());
         System.out.println(circle.getArea());
