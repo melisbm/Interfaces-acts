efae1a3111fb7a11b5e6f67e13a31afdb1f15386
melisbm <miquelangel.melis@gmail.com>
2026-03-15 23:50:00 +0100

    REFACTOR Bank Accounts

--- src/Level2/BankingSystem/CurrentAccount.java
+++ src/Level2/BankingSystem/CurrentAccount.java
@@ -1,50 +1,9 @@
 package Level2.BankingSystem;
 
-public class CurrentAccount implements Account{
+Rpublic class CurrentAccount extends BaseAccount{
 
-    private double balance = 0;
-
-    public CurrentAccount(double amount){
-        if(amount <= 0){
-            throw new IllegalArgumentException("Amount can not be less or equal to 0.");
-        }
-        balance = amount;
+    public CurrentAccount(){
     }
 
-    @Override
-    public void deposit(double amount){
-        if(amount <= 0){
-            throw new IllegalArgumentException("Amount can not be less or equal to 0.");
-        }
-
-        balance += amount;
-    }
-    @Override
-    public void withdraw(Account account, double amount){
-
-        if(this.getBalance() < amount){
-            throw new IllegalArgumentException("Can not withdraw an amount less than the actual balance.");
-        }
-
-        account.deposit(amount);
-        this.balance -= amount;
-    }
-
-    @Override
-    public double calculateInterest(double percentile){
-        if(percentile < 0){
-            throw new IllegalArgumentException("Percentile can not be negative.");
-        }
-        return balance * percentile / 100;
-    }
-
-    @Override
-    public void viewBalance(){
-        System.out.println(balance);
-    }
-
-    public double getBalance(){
-        return balance;
-    }
-
+    
 }
--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,9 @@ import Level2.BankingSystem.*;
 public class Main {
     public static void main(String[] args){
 
-        CurrentAccount curr = new CurrentAccount(123);
-        SavingsAccount save = new SavingsAccount(60);
-
+        CurrentAccount curr = new CurrentAccount();
+        curr.deposit(100);
         curr.viewBalance();
-        save.viewBalance();
-        System.out.println("-------------");
-        curr.deposit(120);
-        curr.viewBalance();
-        System.out.println("-------------");
-        curr.withdraw(save, 58);
-        curr.viewBalance();
-        save.viewBalance();
-        System.out.println("-------------");
-        System.out.println(curr.calculateInterest(25));
-        System.out.println(save.calculateInterest(30));
 
     }
 }
